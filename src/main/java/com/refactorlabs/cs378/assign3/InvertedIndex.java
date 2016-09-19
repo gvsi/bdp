c8416e7b8e1347a227ea4c4539c543baa2e63238
Giovanni Alcantara <me@giovannialcantara.com>
2016-09-19 08:50:16 -0500

    Implement assign3

--- src/main/java/com/refactorlabs/cs378/assign3/InvertedIndex.java
+++ src/main/java/com/refactorlabs/cs378/assign3/InvertedIndex.java
@@ -1,10 +1,102 @@
 package com.refactorlabs.cs378.assign3;
 
 // Imports
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class InvertedIndex extends Configured implements Tool {
 
     // Map and Reduce classes
+
+	public static class MapClass extends Mapper<LongWritable, Text, Text, VerseArrayWritable> {
+
+		/**
+		 * Local variable "word" will contain the word identified in the input.
+		 * The Hadoop Text object is mutable, so we can reuse the same object and
+		 * simply reset its value as each word in the input is encountered.
+		 */
+		private Text word = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+
+			int i = line.indexOf(' ');
+
+			if (i == -1) return;
+
+			HashSet<String> words_set = new HashSet<>();
+
+			String verse_id = line.substring(0, i);
+			String content = line.substring(i+1, line.length());
+			content = content.toLowerCase()
+					.replaceAll("(\\[\\d+\\])", " $1 ") // Add a space before and after [number], so it's tokenized
+					.replaceAll("\\.|,|--|;|:|\\?|\"|=|_|!|\\(|\\)", " "); // Remove punctuation
+
+			StringTokenizer tokenizer = new StringTokenizer(content);
+
+			while (tokenizer.hasMoreTokens()) {
+				words_set.add(tokenizer.nextToken());
+			}
+
+			String[] verse_arr = {verse_id};
+			VerseArrayWritable verse = new VerseArrayWritable(verse_arr);
+
+			for (String s : words_set) {
+				word.set(s);
+				if (s.equals("perverseness")) {
+					System.out.println("Writing " + word.toString() + " -> " + verse);
+				}
+				context.write(word, verse);
+			}
+		}
+	}
+
+	/**
+	 * The Reduce class for word count.  Extends class Reducer, provided by Hadoop.
+	 * This class defines the reduce() function for the word count example.
+	 */
+	public static class ReduceClass extends Reducer<Text, VerseArrayWritable, Text, VerseArrayWritable> {
+
+		@Override
+		public void reduce(Text key, Iterable<VerseArrayWritable> values, Context context)
+				throws IOException, InterruptedException {
+
+			ArrayList<String> verses = new ArrayList<>();
+
+			for (VerseArrayWritable value : values) {
+				for (String s : value.getVerses()) {
+					verses.add(s);
+				}
+			}
+			String[] verses_arr = new String[verses.size()];
+			verses.toArray(verses_arr);
+			VerseArrayWritable res = new VerseArrayWritable(verses_arr);
+			res.sort();
+
+			context.write(key, res);
+		}
+	}
 
     /**
      * The run method specifies the characteristics of the map-reduce job
@@ -20,10 +112,17 @@ public class InvertedIndex extends Configured implements Tool {
 	job.setJarByClass(InvertedIndex.class);
 
 	// Set the output key and value types (for map and reduce).
+	job.setOutputKeyClass(Text.class);
+	job.setOutputValueClass(VerseArrayWritable.class);
 
 	// Set the map and reduce classes (and combiner, if used).
+	job.setMapperClass(MapClass.class);
+	job.setReducerClass(ReduceClass.class);
+	job.setCombinerClass(ReduceClass.class);
 
 	// Set the input and output file formats.
+	job.setInputFormatClass(TextInputFormat.class);
+	job.setOutputFormatClass(TextOutputFormat.class);
 
 	// Grab the input file and output directory from the command line.
 	FileInputFormat.addInputPaths(job, appArgs[0]);
---
+++ src/main/java/com/refactorlabs/cs378/assign3/VerseArrayWritable.java
@@ -0,0 +1,94 @@
+/**
+ * Created by gvsi on 9/15/16.
+ */
+
+package com.refactorlabs.cs378.assign3;
+
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class VerseArrayWritable implements Writable {
+
+    private String[] verses;
+
+    public String[] getVerses() {
+        return verses;
+    }
+
+    public VerseArrayWritable(){}
+
+    public VerseArrayWritable(String[] verses) {
+        this.verses = verses;
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        // Read the data out in the order it is written
+
+        verses = new String[in.readInt()];
+        for (int i = 0; i < verses.length; i++) {
+            verses[i] = in.readUTF();
+        }
+    }
+
+    public void write(DataOutput out) throws IOException {
+        // Write the data out in the order it is read
+        out.writeInt(verses.length);
+        for (String s : verses) {
+            out.writeUTF(s);
+        }
+    }
+
+    public void sort() {
+        Arrays.sort(verses, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (!o1.matches("([A-Z][a-z]+:\\d+:\\d+)")) {
+                    System.out.println("YOOOO + " + o1);
+                }
+
+                if (!o2.matches("([A-Z][a-z]+:\\d+:\\d+)")) {
+                    System.out.println("YOOOO2 + " + o2);
+                }
+                String parts1[] = o1.split(":");
+                String parts2[] = o2.split(":");
+
+                String book1 = parts1[0];
+                String book2 = parts2[0];
+                int chapter1 = Integer.parseInt(parts1[1]);
+                int chapter2 = Integer.parseInt(parts2[1]);
+                int verse1 = Integer.parseInt(parts1[2]);
+                int verse2 = Integer.parseInt(parts2[2]);
+
+                if (book1.compareTo(book2) > 0) {
+                    return 1;
+                } else if (book1.compareTo(book2) < 0) {
+                    return -1;
+                }
+
+                if (chapter1 > chapter2) {
+                    return 1;
+                } else if (chapter1 < chapter2) {
+                    return -1;
+                }
+
+                if (verse1 > verse2) {
+                    return 1;
+                } else if (verse1 < verse2) {
+                    return -1;
+                }
+
+                return 0;
+            }
+        });
+    }
+
+    public String toString() {
+        return String.join(",", verses);
+    }
+
+}
